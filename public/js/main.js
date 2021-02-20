dcf361b28ef73fa01398460047286c2c8998611c
koooota <eno6k21@gmail.com>
2021-02-20 20:35:01 +0900

    modified

--- public/js/main.js
+++ public/js/main.js
@@ -35,7 +35,7 @@ var app = new Vue({
       this.form.statusMessage = profile.statusMessage; //LINEのステータスメッセージ
     },
     createUser: function() {
-      axios.post(url, obj)
+      axios.post(url,conf,obj)
       .then(function (response) {
         console.log(response);
       })
